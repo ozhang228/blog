6d085c69c6e4ff1327518753ca2231a3a7c9a4e1
Oscar Zhang <oscarzhang228@gmail.com>
2026-07-21 22:37:17 -0500

    more

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,26 +8,9 @@ export default defineConfig({
   integrations: [mdx(), sitemap()],
   fonts: [
     {
-      provider: fontProviders.local(),
-      name: "Atkinson",
+      provider: fontProviders.google(),
+      name: "Atkinson Hyperlegible",
       cssVariable: "--font-atkinson",
-      fallbacks: ["sans-serif"],
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/atkinson-regular.woff"],
-            weight: 400,
-            style: "normal",
-            display: "swap",
-          },
-          {
-            src: ["./src/assets/fonts/atkinson-bold.woff"],
-            weight: 700,
-            style: "normal",
-            display: "swap",
-          },
-        ],
-      },
     },
   ],
 });
